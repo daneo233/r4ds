84c19d2002faf1ad9171a43b287d6432084c0a48
Dane Hager <dane.hager@gmail.com>
2018-01-30 03:19:10 +0000

    commit after added graphs

--- Script.R
+++ Script.R
@@ -10,3 +10,14 @@ gapminder_us <- gapminder %>%
   arrange(desc(year))
 ggplot(data=gapminder_us) +
   geom_point(mapping=aes(x=year, y=lifeExp))
+gapminder %>% 
+  count(country)
+
+
+ggplot(data = gapminder_2007) +
+  geom_histogram(mapping = aes(x = lifeExp), binwidth = 2.5)
+gapminder_recent <- gapminder %>% 
+  filter(year>=1980)
+
+gapminder_2007 <- gapminder %>% 
+  filter(year==2007)
