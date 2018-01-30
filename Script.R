6775b3114ebba490f5510455c290202a7ebe50a8
Dane Hager <dane.hager@gmail.com>
2018-01-30 02:56:34 +0000

    Commit 1/29/2018

--- Script.R
+++ Script.R
@@ -1,8 +1,12 @@
+
+# to run in shell - git config --global user.email "dane.hager@gmail.com"
+# to run in shell - git config --global user.name "Dane Hager"
+
 library(gapminder)
 install.packages("gapminder")
 gapminder
-gapminder %>% 
+gapminder_us <- gapminder %>% 
   filter(country=="United States") %>% 
   arrange(desc(year))
-ggplot(gapminder) %>% 
-  geom_line(aes(x=year, y=lifeExp))
+ggplot(data=gapminder_us) +
+  geom_point(mapping=aes(x=year, y=lifeExp))
